0a19693ce4d19ce9ac8a1af260bd36221d944648
Tevtongermany <alexm3350@gmail.com>
2025-05-09 23:55:12 +0200

    Faith 1.0 Release

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Faith
-Faith is a request Redirector to redirect traffic from sony's scehttp lib to your own custom server
+Faith is a request redirector that reroutes traffic from Sony's scehttp lib to your own custom server.
+
+## How Does This Work?
+We hook into sceHttpCreateConnectionWithURL and sceHttpCreateRequestWithURL, then modify the URL passed to those functions with a custom one.
+
+## How to Compile
+
+Get VMWare or Use a real Linux machine  
+Download the OpenOrbis toolchain from this [link](https://github.com/OpenOrbis/OpenOrbis-PS4-Toolchain/releases/tag/v0.5.3) and GoldHen SDK from this [link](https://github.com/GoldHEN/GoldHEN_Plugins_SDK) and
+Store them in a location such as home/(YourUserName)/ in my case I made a folder called ps4jb with both of those folders    
+You would also need to Install the dependenies needed by the OO Toolchain
+```
+sudo apt update
+sudo apt install clang
+sudo apt install lld
+```
+If you are on a arch based distro use:  
+```
+sudo pacman -Sy clang
+sudo pacman -Sy lid
+```
+So after you have that installed you would need to actually expose both the toolchain and the goldhen SDK, simply run nano ~/.bashrc, dont be scared its just a simple text editor, use your arrow keys to go to the botton of the file and you would need to add these lines
+```
+# Change those based on where you put both toolchain and sdk in
+export OO_PS4_TOOLCHAIN=~/ps4jb/PS4Toolchain
+export GOLDHEN_SDK=~/ps4jb/GoldHEN_Plugins_SDK
+```
+press Ctrl + X then press Y and then enter. run source ~./bashrc to refresh it      
+Now the fun part comes, head into plugin_src/Faith/Source and edit the main.cpp, edit the `RedirectURL` to what ever server you want the requests to go to as an example I choose `http://192.168.14:3551`.    
+After thats done you can finally compile! open your command prompt and type in `make` in the Faith Directory where the Makefile Is located at, After a few seconds it should be done building, all build output is stored in bin/plugins/prx_final/ in the root
+of the folder    
+Okay so after you successfully build your own Faith.prx you can simply load it up with goldhen!, simply open up your FTP client of your choice and select your PS4, then Head to /data/GoldHEN/plugins/ and just drag in the Faith.prx  
+after thats done you would need to edit your Plugins.ini, if the plugins folder and the Plugins.ini is missing from the goldhen folder, simply just create them and the content of the Plugins.ini should look like this  
+
+```
+[default]
+/data/GoldHEN/plugins/game_patch.prx
+/data/GoldHEN/plugins/no_share_watermark.prx
+
+[CUSA07669] <-- Change the CUSA Id depending on what Region version of fortnite you have
+/data/GoldHEN/plugins/FaithDebug.prx=true
+```
+
+and now you are done! simply load up fortnite and it should automatically redirect all traffic to your own backend
+
+
+# License Disclosure
+Portions of the materials used are trademarks and/or copyrighted works of Epic Games, Inc. All rights reserved by Epic. This material is not official and is not endorsed by Epic.
---
+++ Source/main.cpp
@@ -0,0 +1,234 @@
+#include <Common.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include <orbis/Pad.h>
+#include <orbis/UserService.h>
+#include <orbis/SystemService.h>
+#include <orbis/Sysmodule.h>
+#include <orbis/Http.h>
+#include <orbis/Ssl.h>
+#include <orbis/Net.h>
+
+// Pyrite Includes
+#include "core.h"
+#include "Logging/public/Log.h"
+
+
+#define PLUGIN_NAME "Faith"
+#define PLUGIN_DESC "Faith is a request Redirector to redirect traffic from sonys scehttp lib to your own custom server "
+#define PLUGIN_AUTH "Tevtongermany"
+#define PLUGIN_VER 0x100 // 1.00
+
+DECLARE_LOG_CATEGORY(LogFaith);
+
+const char *RedirectURL = "https://retrac.site";
+
+
+PUBLIC_ATTRIBUTE const char *g_pluginName = PLUGIN_NAME;
+
+
+PUBLIC_ATTRIBUTE const char *g_pluginDesc = PLUGIN_DESC;
+
+
+PUBLIC_ATTRIBUTE const char *g_pluginAuth = PLUGIN_AUTH;
+
+
+PUBLIC_ATTRIBUTE u32 g_pluginVersion = PLUGIN_VER;
+
+// Hook declarations
+// Redirect
+HOOK_INIT(sceHttpCreateConnectionWithURL);
+HOOK_INIT(sceHttpCreateRequestWithURL);
+
+// Cert Verify
+HOOK_INIT(CERT_validateCertificate);
+HOOK_INIT(CERT_validateCertificateWithConf);
+HOOK_INIT(CA_MGMT_verifyCertWithKeyBlob);
+HOOK_INIT(CA_MGMT_verifySignature);
+HOOK_INIT(CERT_verifySignature);
+HOOK_INIT(RSA_verifySignature);
+HOOK_INIT(CERT_isRootCertificate);
+HOOK_INIT(CERT_isRootCertificate2);
+
+
+int CERT_validateCertificate_hook(void *cert, void *unused) {
+    LOG(LogFaith,LogVerbosity::Log,"Bypassing certificate validation");
+    return 0; 
+}
+
+int CERT_validateCertificateWithConf_hook(void *cert, void *conf) {
+    LOG(LogFaith,LogVerbosity::Log,"Bypassing certificate validation with config");
+    return 0;
+}
+
+int CA_MGMT_verifyCertWithKeyBlob_hook(void *cert, void *keyBlob) {
+    LOG(LogFaith,LogVerbosity::Log,"Bypassing cert verification with key blob");
+    return 0;
+}
+
+int CA_MGMT_verifySignature_hook(void *cert, void *signature) {
+    LOG(LogFaith,LogVerbosity::Log,"Bypassing signature verification");
+    return 0;
+}
+
+int CERT_verifySignature_hook(void *cert, void *signature) {
+    LOG(LogFaith,LogVerbosity::Log,"Bypassing cert signature verification");
+    return 1;
+}
+
+int RSA_verifySignature_hook(void *cert, void *signature) {
+    LOG(LogFaith,LogVerbosity::Log,"Bypassing RSA signature verification");
+    return 1;
+}
+
+int CERT_isRootCertificate_hook(void *cert) {
+    LOG(LogFaith,LogVerbosity::Log,"Forcing root certificate acceptance");
+    return 1; 
+}
+
+int CERT_isRootCertificate2_hook(void *cert) {
+    LOG(LogFaith,LogVerbosity::Log,"Forcing root certificate acceptance (v2)");
+    return 1;
+}
+
+const char *extract_path(const char *url) {
+    if (!url) return NULL;
+    const char *start = strstr(url, "://");
+    if (!start) return NULL;
+    start += 3;
+    const char *path = strchr(start, '/');
+    return path ? path : "";
+}
+
+char *GetUrltoRedirect(const char *url) {
+    if (!url) return NULL;
+
+    if (strstr(url, "ol.epicgames.com") != NULL || 
+        strstr(url, "epicgames.dev") != NULL ||
+        strstr(url, "ol.epicgames.net") != NULL ||
+        strstr(url, ".akamaized.net") != NULL ||
+        strstr(url, "on.epicgames.com") != NULL ||
+        strstr(url, "game-social.epicgames.com") != NULL ||
+        strstr(url, "superawesome.com") != NULL || 
+        strstr(url, "ak.epicgames.com") != NULL ) 
+    {
+        const char *Path = extract_path(url);
+        if (!Path) return NULL;
+
+        size_t newUrlSize = strlen(RedirectURL) + strlen(Path) + 1;
+        char *newRedirectUrl = (char *)malloc(newUrlSize);
+        if (!newRedirectUrl) return NULL;
+
+        strcpy(newRedirectUrl, RedirectURL);
+        strcat(newRedirectUrl, Path);
+        return newRedirectUrl; 
+    }
+
+    return NULL;
+}
+
+
+
+
+int32_t sceHttpCreateRequestWithURL_hook(int32_t conectId, int32_t method, const char *url, uint64_t contentLength) {
+    char * newRedirectUrl = GetUrltoRedirect(url);
+
+    if (newRedirectUrl) {       
+        LOG(LogFaith,LogVerbosity::Log,"Redirecting CreateRequest URL:");
+        LOG(LogFaith,LogVerbosity::Log,"Original: %s", url);
+        LOG(LogFaith,LogVerbosity::Log,"Redirected: %s", newRedirectUrl);
+        
+        int32_t result = HOOK_CONTINUE(sceHttpCreateRequestWithURL, 
+            int32_t(*)(int32_t, int32_t, const char *, uint64_t), 
+            conectId, method, newRedirectUrl, contentLength);
+        
+        free(newRedirectUrl);
+        return result;
+    } 
+    
+    return HOOK_CONTINUE(sceHttpCreateRequestWithURL, 
+        int32_t(*)(int32_t, int32_t, const char *, uint64_t), 
+        conectId, method, url, contentLength);
+}
+
+int32_t sceHttpCreateConnectionWithURL_hook(int32_t templateId, const char *url, bool isKeepalive) {
+    char * newRedirectUrl = GetUrltoRedirect(url);
+    
+    if (newRedirectUrl) {
+        
+        LOG(LogFaith,LogVerbosity::Log,"Redirecting CreateConnection URL:");
+        LOG(LogFaith,LogVerbosity::Log,"Original: %s", url);
+        LOG(LogFaith,LogVerbosity::Log,"Redirected: %s", newRedirectUrl);
+        
+        int32_t result = HOOK_CONTINUE(sceHttpCreateConnectionWithURL, 
+            int32_t(*)(int32_t, const char *, bool), 
+            templateId, newRedirectUrl, isKeepalive);
+        
+        free(newRedirectUrl);
+        return result;
+    }
+
+    return HOOK_CONTINUE(sceHttpCreateConnectionWithURL, 
+        int32_t(*)(int32_t, const char *, bool), 
+        templateId, url, isKeepalive);
+}
+
+
+
+extern "C" {
+    
+    PUBLIC_ATTRIBUTE s32 plugin_load(s32 argc, const char* argv[])  {
+        LOG(LogFaith,LogVerbosity::Log,"[GoldHEN] %s Plugin Started.", g_pluginName);
+        LOG(LogFaith,LogVerbosity::Log,"[GoldHEN] <%s\\Ver.0x%08x> %s", g_pluginName, g_pluginVersion, __func__);
+        LOG(LogFaith,LogVerbosity::Log,"[GoldHEN] Plugin Author(s): %s", g_pluginAuth);
+        
+        LOG(LogFaith,LogVerbosity::Log,"Hooking into Request Functions");
+        HOOK32(sceHttpCreateConnectionWithURL);
+        HOOK32(sceHttpCreateRequestWithURL);
+
+        LOG(LogFaith,LogVerbosity::Log,"Hooking into Cert validation Functions");
+        HOOK32(CERT_validateCertificate);
+        HOOK32(CERT_validateCertificateWithConf);
+        HOOK32(CA_MGMT_verifyCertWithKeyBlob);
+        HOOK32(CA_MGMT_verifySignature);
+        HOOK32(CERT_verifySignature);
+        HOOK32(RSA_verifySignature);
+        HOOK32(CERT_isRootCertificate);
+        HOOK32(CERT_isRootCertificate2);
+        
+        return 0;
+    } 
+
+
+    
+    PUBLIC_ATTRIBUTE s32 plugin_unload(s32 argc, const char* argv[]) {
+        LOG(LogFaith,LogVerbosity::Log,"[GoldHEN] <%s\\Ver.0x%08x> %s", g_pluginName, g_pluginVersion, __func__);
+        LOG(LogFaith,LogVerbosity::Log,"[GoldHEN] %s Plugin Ended.", g_pluginName);
+
+        UNHOOK(sceHttpCreateConnectionWithURL);
+        UNHOOK(sceHttpCreateRequestWithURL);
+        
+        UNHOOK(CERT_validateCertificate);
+        UNHOOK(CERT_validateCertificateWithConf);
+        UNHOOK(CA_MGMT_verifyCertWithKeyBlob);
+        UNHOOK(CA_MGMT_verifySignature);
+        UNHOOK(CERT_verifySignature);
+        UNHOOK(RSA_verifySignature);
+        UNHOOK(CERT_isRootCertificate);
+        UNHOOK(CERT_isRootCertificate2);
+
+        return 0;
+    }
+
+    
+    WEAK_ATTRIBUTE s32 module_start(s64 argc, const void *args) {
+        return 0;
+    }
+
+
+    WEAK_ATTRIBUTE s32 module_stop(s64 argc, const void *args) {
+        return 0;
+    }
+
+} 
